0e3cf221d91f407d2b5ba33444ce8280b4e47b29
Agustin <agustinrc101@gmail.com>
2018-03-07 16:29:40 -0800

    Finished 3D minimap (basic)
    Working copy of 3D minimap, should work with ovrcaemra and player object

---
+++ Space Tech Demo/Assets/_Project/Scripts/MinimapPlayerBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapPlayerBehavior : MonoBehaviour {
+	[SerializeField] private Transform _area;
+	[SerializeField] private GameObject _player;
+	[SerializeField] private GameObject _camera;
+	[SerializeField] private float _hologramScale;
+	[SerializeField] private float _yPos;
+
+	void Update () {
+		setPosition();
+		setRotation();
+	}
+
+	private void setPosition() {
+		//Player's current location
+		Vector3 _playerLocation = _player.transform.position;
+
+		//Position relative to player and area
+		Vector3 playerPos = _playerLocation - _area.position;
+
+		//Minimap's position
+		Vector3 parentPos = transform.parent.position;
+
+		//Approximated position
+		Vector3 newPos = parentPos + (playerPos * _hologramScale);
+		newPos = new Vector3(newPos.x, parentPos.y + _yPos, newPos.z);
+
+		transform.position = newPos;
+	}
+
+	private void setRotation() {
+		transform.rotation = _camera.transform.rotation;
+	}
+}
